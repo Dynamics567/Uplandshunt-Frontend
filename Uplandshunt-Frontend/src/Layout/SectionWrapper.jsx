e8266ef33317d4b0648a13de95a2c2ac6eec9253
folarmi <fola4ril@gmail.com>
2021-06-29 17:53:02 +0100

    initial commit

--- Uplandshunt-Frontend/src/Layout/SectionWrapper.jsx
+++ Uplandshunt-Frontend/src/Layout/SectionWrapper.jsx
@@ -1,6 +1,6 @@
 const SectionWrapper = ({ children, className }) => {
   return (
-    <section className={`container m-auto w-11/12 mt-8  ${className}`}>
+    <section className={`container m-auto w-11/12 mt-4  ${className}`}>
       {children}
     </section>
   );
