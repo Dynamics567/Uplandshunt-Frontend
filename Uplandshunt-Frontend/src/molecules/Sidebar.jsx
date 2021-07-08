6b624dae1cc500e44b0724023291c91d8d23478c
folarmi <fola4ril@gmail.com>
2021-07-08 09:52:04 +0100

    worked on user view page

--- Uplandshunt-Frontend/src/molecules/Sidebar.jsx
+++ Uplandshunt-Frontend/src/molecules/Sidebar.jsx
@@ -9,21 +9,23 @@ const Sidebar = () => {
   return (
     <div className="bg-primary h-full px-6 w-64 text-white">
       <p className="font-normal py-6 text-base text-center">Sample Logo</p>
-      {userDashboard.map(({ id, image, navItem, link }) => {
-        return (
-          <NavLink
-            activeClassName="active"
-            to={link}
-            className="flex items-center cursor-pointer mb-20"
-            key={id}
-            onClick={scrollToTop}
-          >
-            <img src={image} alt={navItem} className="mr-4" />
-            <p className="font-bold text-base">{navItem}</p>
-          </NavLink>
-        );
-      })}
-      <p className="font-bold text-white text-base">Logout</p>
+      <div className="flex flex-col h-full">
+        {userDashboard.map(({ id, image, navItem, link }) => {
+          return (
+            <NavLink
+              activeClassName="active"
+              to={link}
+              className="flex items-center cursor-pointer mb-20"
+              key={id}
+              onClick={scrollToTop}
+            >
+              <img src={image} alt={navItem} className="mr-4" />
+              <p className="font-bold text-base">{navItem}</p>
+            </NavLink>
+          );
+        })}
+        <p className="font-bold text-white ml-10 text-base">Logout</p>
+      </div>
     </div>
   );
 };
