2c700d814d242458e9c660f846cf62ee969bf7fd
folarmi <fola4ril@gmail.com>
2021-07-12 10:12:27 +0100

    perfected routing

--- Uplandshunt-Frontend/src/App.js
+++ Uplandshunt-Frontend/src/App.js
@@ -2,51 +2,63 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import { ProtectedRoute } from "./Auth/ProtectedRoute";
-import routes from "./Config/routes";
+// import routes from "./Config/routes";
 import { AuthProvider } from "./Context";
-import { Dashboard } from "./pages";
 
-// import {
-//   IndividualSignup,
-//   ResetPassword,
-//   Login,
-//   PasswordRecovery,
-//   Landing,
-//   DashboardLogin,
-//   Blog,
-//   Regulations,
-//   News,
-//   SingleNews,
-//   SingleBlog,
-//   SingleRegulation,
-//   ContactUs,
-//   About,
-//   PrivacyPolicy,
-//   TermsAndConditions,
-//   Dashboard,
-// } from "./pages";
+import {
+  IndividualSignup,
+  ResetPassword,
+  Login,
+  PasswordRecovery,
+  Landing,
+  DashboardLogin,
+  Blog,
+  Regulations,
+  News,
+  SingleNews,
+  SingleBlog,
+  SingleRegulation,
+  ContactUs,
+  About,
+  PrivacyPolicy,
+  TermsAndConditions,
+  Dashboard,
+} from "./pages";
 
 function App() {
   return (
     <div className="overflow-hidden">
       <AuthProvider>
         <Router>
-          {/* <Switch> */}
-          {routes.map((route) => (
-            <Route
-              exact
-              key={route.path}
-              path={route.path}
-              component={route.component}
-              isPrivate={route.isPrivate}
-            />
-          ))}
-          {/* </Switch> */}
-        </Router>
-      </AuthProvider>
-      <AuthProvider>
-        <Router>
-          <ProtectedRoute path="/dashboard" component={Dashboard} />
+          <Switch>
+            <div className="">
+              <Route path="/" exact component={Landing} />
+              <Route path="/register" exact component={IndividualSignup} />
+              <Route path="/resetpassword" exact component={ResetPassword} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/contactus" exact component={ContactUs} />
+              <Route
+                path="/forgotpassword"
+                exact
+                component={PasswordRecovery}
+              />
+              <Route path="/dashboardlogin" exact component={DashboardLogin} />
+              <Route path="/legal/news" component={News} />
+              <Route path="/about" exact component={About} />
+              <Route path="/privacypolicy" exact component={PrivacyPolicy} />
+              <Route path="/terms" exact component={TermsAndConditions} />
+              <ProtectedRoute path="/dashboard" component={Dashboard} />
+              <Route path="/legal/regulations" exact component={Regulations} />
+              <Route path="/legal/blog" exact component={Blog} />
+              <Route path="/legal/newsview" exact component={SingleNews} />
+              <Route path="/legal/blogview" component={SingleBlog} />
+              <Route
+                path="/legal/regulationview"
+                exact
+                component={SingleRegulation}
+              />
+            </div>
+          </Switch>
         </Router>
       </AuthProvider>
     </div>
