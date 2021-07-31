b7d277d3e83dab6c760c95c4fb5ab674d7ebf637
folarmi <fola4ril@gmail.com>
2021-07-31 03:15:58 +0100

    finished property details page

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
+import { HeaderTwo } from "../molecules";
 import prop3 from "../assets/prop3.svg";
+import smallProp from "../assets/smallProp.png";
+import interest from "../assets/interest.png";
+import bid from "../assets/bid.png";
+import love from "../assets/love.png";
+import bidIcon from "../assets/bidIcon.png";
+import location from "../assets/location.svg";
 import listed from "../assets/listed.png";
 import searchBy from "../assets/searchBy.png";
 import detailsBedroom from "../assets/detailsBed.png";
@@ -10,10 +17,11 @@ import detailsKitchen from "../assets/detailsKitchen.png";
 import detailsParking from "../assets/detailsParking.png";
 import { axiosInstance } from "../Auth/Axios";
 import DashboardLoader from "../templates/DashboardLoader";
+import { Footer } from "../organisms";
 
 const PropertyDetails = () => {
   let { id } = useParams();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [propertyDetails, setPropertyDetails] = useState([]);
 
   const getPropertyDetails = () => {
@@ -24,6 +32,7 @@ const PropertyDetails = () => {
         const details = response.data.data;
         setPropertyDetails(details);
         console.log(details);
+        console.log();
         setLoading(false);
       })
       .catch(function (error) {
@@ -39,7 +48,29 @@ const PropertyDetails = () => {
     getPropertyDetails();
   }, []);
 
-  const { name, price, city, country, postal_code, status } = propertyDetails;
+  if (loading) {
+    return <DashboardLoader />;
+  }
+
+  const {
+    name,
+    price,
+    city,
+    country,
+    area,
+    postal_code,
+    status,
+    geo_location: {
+      geometry: {
+        location: { lat, lng },
+      },
+    },
+    // availability_status: { available: name },
+    kitchens: { kitchen },
+    bath: { bathroom },
+    bed: { bedroom },
+    // bed: { bedroom },
+  } = propertyDetails;
 
   return (
     <div>
@@ -47,37 +78,86 @@ const PropertyDetails = () => {
         <DashboardLoader />
       ) : (
         <>
-          <div className="m-auto mt-10 w-11/12 grid grid-cols-2">
-            <div className="">
-              <p>{name}</p>
-              <img
-                src={prop3}
-                alt="propertyPicture"
-                className="object-contain"
-              />
-              <div className="flex w-32 h-20">
-                <img src={detailsBedroom} alt="detailsBedroom" className="" />
-                <img src={detailsKitchen} alt="detailsKitchen" />
-                <img src={detailsBathroom} alt="detailsBathroom" />
-                <img src={detailsParking} alt="detailsParking" />
+          <HeaderTwo />
+          <div className="m-auto mt-10 w-11/12 flex justify-between">
+            <div className="mr-10" style={{ flex: "1" }}>
+              <p className="font-bold text-base">{name}</p>
+              <div className="w-full flex justify-between my-2">
+                <section className="flex items-center">
+                  <img src={location} alt="location" className="mr-2" />
+                  <p className="font-semibold text-sm">{city}</p>
+                </section>
+                <p className="font-bold text-base">{price}/Year</p>
+              </div>
+              <div className="">
+                <img
+                  src={prop3}
+                  alt="propertyPicture"
+                  className="object-cover w-full"
+                />
+                <div className="flex mt-2 w-full">
+                  <img src={smallProp} alt="smallProp" className="w-1/4 mr-2" />
+                  <img src={smallProp} alt="smallProp" className="w-1/4 mr-2" />
+                  <img src={smallProp} alt="smallProp" className="w-1/4 mr-2" />
+                  <img src={smallProp} alt="smallProp" className="w-1/4 mr-2" />
+                </div>
+              </div>
+              <div className="flex w-full mt-2">
+                <img
+                  src={detailsBedroom}
+                  alt="detailsBedroom"
+                  className="w-1/4"
+                />
+                <img
+                  src={detailsKitchen}
+                  alt="detailsKitchen"
+                  className="w-1/4"
+                />
+                <img
+                  src={detailsBathroom}
+                  alt="detailsBathroom"
+                  className="w-1/4"
+                />
+                <img
+                  src={detailsParking}
+                  alt="detailsParking"
+                  className="w-1/4"
+                />
+              </div>
+              <div className="flex mt-2">
+                <div className="flex px-16 py-2 items-center justify-center rounded-md border border-primary mr-8">
+                  <img src={love} alt="love" className="w-4 mr-4" />
+                  <p className="text-primary text-base font-bold">Save</p>
+                </div>
+                <div className="bg-primary flex px-16 py-2 items-center justify-center rounded-md border border-primary">
+                  <img src={bidIcon} alt="bidIcon" className="w-4 mr-4" />
+                  <p className="text-white text-base font-bold">Bid</p>
+                </div>
               </div>
             </div>
-            <div className="">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla
-              voluptatem excepturi unde illo incidunt placeat, dolorum ratione!
-              Quod perferendis voluptatibus laboriosam atque nihil. Nemo, error,
-              rerum enim nostrum sequi accusantium nihil quidem eligendi
-              <div className="flex w-56 h-36">
-                <img src={listed} alt="listed" />
-                <img src={searchBy} alt="" />
+            <div className="" style={{ flex: "1" }}>
+              <div className="w-full h-full mt-16">
+                <iframe
+                  src={`https://maps.google.com/maps?q=${lat}, ${lng}&z=15&output=embed`}
+                  width="100%"
+                  height="70%"
+                  frameborder="0"
+                  style={{ width: "100%", objectFit: "cover" }}
+                ></iframe>
+                <div className="flex w-full mt-4">
+                  <img src={listed} alt="listed" className="w-1/2" />
+                  <img src={searchBy} alt="searchBy" className="w-1/2" />
+                </div>
               </div>
             </div>
-            voluptas nesciunt inventore obcaecati deleniti molestiae magni.
+            {/* voluptas nesciunt inventore obcaecati deleniti molestiae magni. */}
           </div>
-          <section className="m-auto w-11/12">
-            <p>Details</p>
-            <p>Property Description</p>
-            <p>
+          <section className="m-auto mt-16 w-11/12 border border-lightAsh">
+            <p className="border-b border-ashThree font-bold text-lg p-4">
+              Details
+            </p>
+            <p className="p-4 font-bold text-lg">Property Description</p>
+            <p className="p-4 font-normal text-base">
               Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam eaque ipsa,
               quae ab illo inventore veritatis et quasi architecto beatae vitae
@@ -100,32 +180,48 @@ const PropertyDetails = () => {
               quidem rerum facilis est et expedita distinctio. Nam libero
               tempore
             </p>
-            <p>Property Details</p>
-            <div className="grid grid-cols-4 gap-4">
+            <p className="px-4 py-2 font-bold text-lg">Property Details</p>
+            <div className="grid grid-cols-4 gap-4 p-4 text-base font-semibold">
               <div className="">
-                <p>Postal code:{postal_code}</p>
-                <p>Availability:</p>
-                <p>Date:</p>
+                <p>Postal code: {postal_code}</p>
+                <p>Availability: </p>
+                <p>Date: </p>
               </div>
               <div className="">
-                <p>Amenities:</p>
-                <p>Listing Type:</p>
-                <p>Area:</p>
+                <p>
+                  Amenities:
+                  {/* <div className="flex">
+                    {propertyDetails.amenities.map(({ id, name }) => {
+                      return (
+                        <div className="" key={id}>
+                          <p>{name},</p>
+                        </div>
+                      );
+                    })}
+                  </div> */}
+                </p>
+                <p>Listing Type: </p>
+                <p>Area: {area}sqm</p>
               </div>
               <div className="">
-                <p>No of Bedrooms:</p>
-                <p>No of Bathrooms:</p>
-                <p>Kitchen:</p>
+                <p>No of Bedrooms: {bedroom.slice(0, 1)}</p>
+                <p>No of Bathrooms: {bathroom.slice(0, 1)}</p>
+                <p>Kitchen: {kitchen.slice(0, 1)}</p>
               </div>
               <div className="">
-                <p>Postal code:{postal_code}</p>
-                <p>Selling Status:{status}</p>
-                <p>Furnishing Type:</p>
+                <p>Postal code: {postal_code}</p>
+                <p>Selling Status: {status}</p>
+                <p>Furnishing Type: </p>
               </div>
             </div>
           </section>
         </>
       )}
+      {/* <div className="p-4 flex w-full">
+        <img src={interest} alt="interest" className="w-1/2" />
+        <img src={bid} alt="bid" className="w-1/2" />
+      </div> */}
+      <Footer />
     </div>
   );
 };
