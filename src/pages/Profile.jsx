7f6e4efeef9381fae394a98112a16e4197895869
folarmi <fola4ril@gmail.com>
2021-08-04 03:49:56 +0100

    differentiated between individual and business profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,104 +1,11 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-import edit from "../assets/userDashboard/edit.svg";
-import { axiosWithAuth } from "../Auth/Axios";
-import DashboardLoader from "../templates/DashboardLoader";
+import BusinessProfile from "../templates/BusinessProfile";
+import IndividualProfile from "../templates/IndividualProfile";
 
 const Profile = () => {
-  const [response, setResponse] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const getUserDetails = JSON.parse(localStorage.getItem("auth"));
+  const getUserType = getUserDetails.user.business;
 
-  const getUserProfile = () => {
-    setLoading(true);
-    axiosWithAuth()
-      .get("auth/me")
-      .then(function (response) {
-        const profile = response.data.data;
-        setResponse(profile);
-        setLoading(false);
-      })
-      .catch(function (error) {
-        if (error.response) {
-          setError(error.response.data.data);
-          setLoading(false);
-        }
-        // handle error
-        setError(error.status);
-      });
-  };
-
-  useEffect(() => {
-    getUserProfile();
-  }, []);
-
-  const { email, first_name, phone, avatar, last_name, address } = response;
-
-  const userObject = [
-    ["Name", `${first_name} ${last_name}`],
-    ["Email Address", email],
-    ["Contact Number", phone],
-    ["Address", address],
-  ];
-
-  return (
-    <>
-      {loading ? (
-        <DashboardLoader />
-      ) : (
-        <div className="">
-          <div className="flex mb-20">
-            <div className="flex-none mr-6 ml-10 mt-6">
-              <img src={avatar} alt="profileAvatar" />
-              <p className="font-bold text-2xl mb-4"></p>
-              <p className="font-semibold text-base"></p>
-            </div>
-
-            <div className="flex-grow w-full">
-              <div className="border rounded-md mt-6 ml-10 w-3/4">
-                <div className="bg-primary flex justify-between p-4 text-white">
-                  <p className="font-bold text-base">Personal Information</p>
-                  <Link to="/dashboard/editprofile" className="cursor-pointer">
-                    <img src={edit} alt="edit" />
-                  </Link>
-                </div>
-                <div
-                  className=""
-                  style={{ boxShadow: "0px 0px 4px 0px #00000040" }}
-                >
-                  {userObject.map((item, index) => {
-                    return (
-                      <div
-                        className="flex p-4"
-                        key={index}
-                        style={{
-                          borderBottom: "0.7px solid #c4c4c4",
-                        }}
-                      >
-                        <p
-                          className="font-semibold text-base"
-                          style={{ flex: "1" }}
-                        >
-                          {item[0]}
-                        </p>
-                        <p
-                          className="font-normal text-lg mr-4"
-                          style={{ flex: "1" }}
-                        >
-                          {item[1]}
-                        </p>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+  return <>{getUserType ? <BusinessProfile /> : <IndividualProfile />}</>;
 };
 
 export { Profile };
