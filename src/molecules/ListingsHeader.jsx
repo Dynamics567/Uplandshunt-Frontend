2c980699344337cbc8610f16b53fd46488dd1c45
folarmi <fola4ril@gmail.com>
2021-08-06 13:47:45 +0100

    fixed listings header

--- src/molecules/ListingsHeader.jsx
+++ src/molecules/ListingsHeader.jsx
@@ -1,15 +1,18 @@
+import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
 import { listingHeader } from "../data/subscription";
 
 const ListingsHeader = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <div className="flex items-center justify-between">
-      {listingHeader.map(({ id, item, url }) => {
+      {listingHeader.map(({ item, url }) => {
         return (
           <NavLink
             className="cursor-pointer"
             activeClassName="dashboard-active"
-            to={url}
+            to={`${url}/${id}`}
             key={id}
           >
             <p className="font-bold text-lg text-ash mr-2">{item}</p>
--- src/pages/UserView.jsx
+++ src/pages/UserView.jsx
@@ -1,14 +1,4 @@
 import { ListingsLayout } from "../Layout";
-// import prop1 from "../assets/userDashboard/smallProp.svg";
-// import location from "../assets/location.svg";
-// import amenities from "../assets/userDashboard/amenities.svg";
-// import map from "../assets/userDashboard/map.svg";
-// import searchBy from "../assets/userDashboard/searchBy.svg";
-// import rentedBy from "../assets/userDashboard/rentedBy.svg";
-// import saveButton from "../assets/userDashboard/saveButton.svg";
-// import shareButton from "../assets/userDashboard/shareButton.svg";
-// import details from "../assets/userDashboard/details.svg";
-// import call from "../assets/userDashboard/call.svg";
 import { PropertyDetails } from "./PropertyDetails";
 
 const UserView = () => {
